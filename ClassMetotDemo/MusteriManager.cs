a8019541315350c128c1172376a55483c67b2dd2
Bilgehan064 <davae0644@gmail.com>
2021-01-20 10:11:14 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void musteriList(Musteri musteri)
+        {
+            Console.WriteLine("Müsteri Bilgileri");
+            Console.WriteLine("-----------------");
+            Console.WriteLine(musteri.Ad);
+            Console.WriteLine(musteri.Soyad);
+            Console.WriteLine(musteri.Yas);
+            Console.WriteLine(musteri.Id);
+            Console.WriteLine("-----------------");
+        }
+
+        public void musteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Eklendi : " + musteri.Ad);
+        }
+
+        public void musteriSil (Musteri musteri)
+        {
+            Console.WriteLine("Musteri Silindi : " + musteri.Ad);
+        }
+            
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Bilgehan";
+            musteri1.Soyad = "Yaradanakul";
+            musteri1.Yas = 18;
+            musteri1.Id = 123456789;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Buğse";
+            musteri2.Soyad = "Ateş";
+            musteri2.Yas = 22;
+            musteri2.Id = 132456784;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Hakkı";
+            musteri3.Soyad = "Yılmaz";
+            musteri3.Yas = 37;
+            musteri3.Id = 132346784;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.musteriList(musteri1);
+            musteriManager.musteriList(musteri2);
+            musteriManager.musteriList(musteri3);
+
+            musteriManager.musteriEkle(musteri1);
+
+            musteriManager.musteriSil(musteri2);
+
+
+
+
+        }
+    }
+}
